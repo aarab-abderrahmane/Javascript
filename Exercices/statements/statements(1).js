d2c57cb03f78c722d3ce8d93e8aac8cde0fb12ff
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-02-11 13:11:55 +0100

    my first javascript #4

---
+++ Exercices/statements/statements(1).js
@@ -0,0 +1,52 @@
+let x,y,z;
+x=10;
+y=5;
+z=4;
+z=x+y;
+
+document.getElementById('demo').innerHTML = z;
+
+
+let person1 = "abderrahmane"
+let person2 = "m9rach"
+
+document.getElementById('ex1').innerHTML = person1
+
+
+let d=6
+let h=7
+
+let $dh= d + h
+document.getElementById('ex2').innerHTML = $dh
+
+
+let Lastname , last_name , fisrtname ;
+Lastname = "Aarab"
+last_name = "aarab"
+
+fisrtname = "abderrahmane"
+
+document.getElementById('ex3').innerHTML = Lastname + " " + fisrtname
+
+
+
+// !Single comment 
+
+
+/*
+!multi line comments
+!multi line comments
+!multi line comments
+*/
+
+let s = 1 ; 
+let f = 2 ; 
+let p = 0  ; 
+
+p=s ;
+s=f;
+f=p;
+
+
+
+document.getElementById('ex4').innerHTML = '<b>s</b>:' + s + ' <b>p</b>:' + p + ' <b>f</b>:' + f
