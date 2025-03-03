54f1e171cd500b789cf3f9ef32477014c406f26d
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-03-03 22:02:26 +0000

    caculator #2

--- Exercices/calculator/calculator.js
+++ Exercices/calculator/calculator.js
@@ -1,3 +1,32 @@
+let output = document.getElementById('result')
+
 function appendValue(value){
-    document.getElementById('result').textContent+=value
+    output.value+=value
+}
+
+function calculate() {
+    output.value= eval(output.value)
+}
+
+function clearOutput(){
+    output.value = ""
+}
+
+
+document.addEventListener('keydown',function(event){
+    const allowedkeys = "0123456789+-*/.=EnterBackspace";
+    if (allowedkeys.includes(event.key)){
+        if (event.key === 'Enter'){
+            calculate();
+        }else if (event.key == 'Backspace'){
+            output.value = output.value.slice(0,-1);
+        }else {
+            appendValue(event.key);
+        }
+    }
+})
+
+
+function backSpace(){
+    output.value = output.value.slice(0,-1);
 }
