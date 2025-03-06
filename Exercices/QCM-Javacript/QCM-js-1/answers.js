8904877e9887d17537ffe77293c0ec1d62ed1540
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-03-06 23:35:01 +0000

    QCM-js-1  #2

--- Exercices/QCM-Javacript/QCM-js-1/answers.js
+++ Exercices/QCM-Javacript/QCM-js-1/answers.js
@@ -67,12 +67,71 @@ outer()
 console.log('___________________________7__________')
 
 
-let myList = [23,41,0,87];
+let myList = [23,41,0,897];
 const R= myList.filter(myfunction);
 
 function myfunction(a){
     let b = `${a}`;
-    return b.length >50;
+    return b.length >2;
 }
 
 console.log(R)
+
+
+console.log('___________________________8__________')
+
+var fruits = ['Banane','orange','Pomme'];
+var x= fruits.pop()
+
+console.log(x)
+console.log(fruits)
+
+
+console.log('___________________________9__________')
+
+console.log(NaN == NaN)
+console.log("infinity"==Infinity)
+console.log("0xa"===10) //because one is a number and the other is a string 
+console.log("0xa"===10) //  this is True , because "0xa" is converted to number (10) before theh comparison
+
+
+console.log('___________________________10__________')
+
+console.log(Math.floor(2.9))
+console.log("3",parseInt('3'))
+console.log(Math.max(-4,3,100,30))
+var o =3 ; console.log(o++)
+var o =3 ; console.log(++o)
+
+
+console.log('___________________________11__________')
+
+let firstParagraph = document.querySelector('p');
+let next = firstParagraph.nextSibling;
+console.log(next);
+
+let firstParagraph_2 = document.querySelector("p");
+let nextElement = firstParagraph_2.nextElementSibling; // Returns the next sibling element node (the second paragraph)
+console.log(nextElement); // Outputs: <p>Second paragraph</p>
+
+let firstParagraph_3 = document.querySelector('.container_2 p');
+let next_2 = firstParagraph_3.nextSibling;
+console.log(next_2)
+
+
+// nextSibling ==> return only the text and comments + if there is white space or a newline between the elements , it will be considered as a text node
+// nexElemntSibling ==> return only the elemnt 
+
+console.log('___________________________12__________')
+
+let ex_node = document.getElementById('node-exemple');
+console.log(ex_node.childNodes)
+console.log(ex_node.children)
+console.log(ex_node.firstChild)
+console.log(ex_node.firstElementChild)
+
+
+//.childNodes	All nodes (comments, text, elements)
+//.children	Only elements (ignores comments & text)
+
+
