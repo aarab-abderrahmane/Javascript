8eb8c7ec12bf5b07c1b24dda4532be6abf60fa93
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-03-27 18:50:05 +0000

    validation-form-2 #2

--- Exercices/validation-des-formulaires/validation-des-formulaire-2/validation-form.js
+++ Exercices/validation-des-formulaires/validation-des-formulaire-2/validation-form.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
+    let emailPettern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;  //    (/.) only '.'   |   []+ at least one 
+    let namePattern = /^[A-Z][a-z]{2,}$/;
+    let passwordPattern = /^[A-Z]{3,}[a-z]{5,}$/;
 
-    document.querySelectorAll('.error').forEach(span=>span.style.display='none');
-
+    //start with /^ ... $/
     const nom = document.getElementById('nom');
     const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const conf_password = document.getElementById('conf-password');
 
     const sports_check = document.getElementById('sports-check');
     const musique_check = document.getElementById('musique-check');
@@ -14,19 +18,10 @@ document.addEventListener('DOMContentLoaded',()=>{
     const contry = document.getElementById('contry');
     const file_image = document.getElementById('file');
 
-
     const soumettre_button = document.getElementById('btn-submit');
 
     //order
-    gestion_error={
-        "nom":"",
-        "email":'',
-        'gender':'',
-        'interet':'',
-        'hobbies':'',
-        'message':'',
-        'contry':'',
-        'file':''}
+    gestion_error={"nom":"","email":'','password':'','gender':'','interet':'','hobbies':'','message':'','contry':'','file':''}
 
 
     if (soumettre_button){
@@ -41,12 +36,23 @@ document.addEventListener('DOMContentLoaded',()=>{
             gestion_error[key]="";
         })
 
-        // console.log(condition.checked)
-
-        !nom.value ? gestion_error['nom']=false : gestion_error['nom']=true;
+        !namePattern.test(nom.value) ? gestion_error['nom']=false : gestion_error['nom']=true;
 
 
-        !email.value? gestion_error['email']=false : gestion_error['email']=true;
+        emailPettern.test(email.value)? gestion_error['email']=true : gestion_error['email']=false;
+        
+        let password_error_messgae = ""
+        if (passwordPattern.test(password.value)){
+
+            if (password.value === conf_password.value){
+                gestion_error['password']=true;
+
+            }else{gestion_error['password']=false;password_error_messgae="Password does not math"}
+
+        }else{
+            gestion_error['password']=false;
+            password_error_messgae="you must enter a strong password"
+        }
 
 
         !gender? gestion_error['gender']=false: gestion_error['gender']=true;
@@ -59,11 +65,9 @@ document.addEventListener('DOMContentLoaded',()=>{
 
         hobbies.value ==='Choose...' ? gestion_error['hobbies'] = false : gestion_error['hobbies']=true;
 
-        !message.value ? gestion_error['message']=false : gestion_error['message']=true;
+        message.value.length<10 ? gestion_error['message']=false : gestion_error['message']=true;
 
         contry.value==="Choose..." ? gestion_error['contry']=false: gestion_error['contry']=true;
-
-
 
         if (file_image.files[0]){
 
@@ -79,20 +83,10 @@ document.addEventListener('DOMContentLoaded',()=>{
             gestion_error['file']=false
         }
 
-        console.log(gestion_error)
 
         const isAlltrue= Object.values(gestion_error).every(value => value===true)
 
-
-        const all_element=[
-            nom,
-            email,
-            "gender",
-            "interet",
-            hobbies,
-            message,
-            contry,
-            file_image]
+        const all_element=[nom,email,document.getElementById('pass-error'),"gender","interet",hobbies,message,contry,file_image]
 
         const all_errrs_classes = document.querySelectorAll('.error');
 
@@ -100,23 +94,74 @@ document.addEventListener('DOMContentLoaded',()=>{
             all_errrs_classes.forEach(el=>el.style.display="none")
             Object.keys(gestion_error).forEach((key,index)=> {
 
-                if (key!=="gender" && key!=="interet"){
+                if (key!=="gender" && key!=="interet" ){
                     all_element[index].classList.remove('not-complete')
+
                 }
             } )
-            
-            all_element.forEach(el=>el.classList.remove('not-complete'))
-        }else{
+
+            const forms = document.getElementById('form-valide');
+
+            const interet=[];
+            if (musique_check.checked){
+                interet.push('Musique')
+            }
+            if (sports_check.checked){
+                interet.push('Sports')
+            }
+            if (lecture_check.checked){
+                interet.push('Lecture')
+            }
+
             
 
+            const div = document.createElement('div');
+            div.classList.add('form');
+
+            
+
+            const reader = new FileReader();
+            reader.readAsDataURL(file_image.files[0])
+
+            reader.onload = function(e){
+
+                div.innerHTML=`
+                <div>
+                    <p>
+                    name : <b>${nom.value}</b><br>
+                    email : <b>${email.value}</b><br>
+                    password : <b>${password.value}</b><br>
+                    sexe : <b>${gender.value}</b><br>
+                    Centres d'intéret : <b>${interet.join(' / ')}</b><br>
+                    Hobbies : <b>${hobbies.value}</b><br>
+                    </p>
+                </div>
+                <div>
+                    <p>
+                    message : <br>
+                        <pre>${message.value}</pre>
+                    </p>
+                </div>
+                <img src='${e.target.result}' width=100 height=100>
+            `   
+                forms.appendChild(div);
+            }
+
+
+            
+        }else{
             Object.keys(gestion_error).forEach((key,index)=>{
 
                 console.log(index,key)
-                console.log("_______________________")
 
-                if(!gestion_error[key]===true && key!=="gender" && key!=="interet"){
+                if(!gestion_error[key]===true  && key!=="gender" && key!=="interet"){
 
-                    all_element[index].classList.add('not-complete')
+                    all_element[index].classList.add('not-complete');
+
+                    if (key==='password'){
+                        all_element[index].textContent=password_error_messgae;
+
+                    }
                         
                 }else if(gestion_error[key]===true && key!=="gender" && key!=="interet"){
                     all_element[index].classList.remove('not-complete')
@@ -124,7 +169,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 
                 if(!gestion_error[key]===true){
 
-                    all_errrs_classes[index].style.display="inline"
+                    all_errrs_classes[index].style.display="inline";
 
                 }else{
                     all_errrs_classes[index].style.display="none";
