55e58bbb2ee0f2db24b6a9d68353c1db90629088
abdulrahman <abdulrahman.tie@gmail.com>
2025-05-22 18:13:01 +0100

    exam2

--- Exercices/exams/exam2/exercice2.js
+++ Exercices/exams/exam2/exercice2.js
@@ -1,1 +1,33 @@
+class Véhicule{
 
+    constructor(marque,modéle,prix_location){
+
+        this.marque = marque ;
+        this.modéle = modéle;
+        this.prix_location = prix_location;
+
+    }
+
+    affiche(){
+        return `marque : ${this.marque} , modéle : ${this.modéle} , prix de location ${this.prix_location}`;
+    }
+
+}
+
+class Voiture extends Véhicule {
+
+    constructor(marque,modéle , prix_location , nbr_portes){
+        super(marque,modéle,prix_location);
+        this.nbr_portes = nbr_portes;
+
+    }
+
+    affiche(){
+        return ` ${super.affiche()} , nombre de portes : ${this.nbr_portes} `
+    }
+    
+    }
+
+
+const voiture1 = new Voiture("dacia","40000","5")
+console.log(voiture1.affiche())
